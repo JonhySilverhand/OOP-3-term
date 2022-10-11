5fd6224060a502e356d4a66821a13b5ac5575282
Vlad Smirnov <smirnovvlad313@gmail.com>
2022-10-11 17:37:40 +0300

    Лабораторная работа №5

---
+++ Lab05/Lab05/Candies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab05
+{
+    class Candies
+    {
+        private struct CandiesSpecification
+        {
+            string type;
+            string AmountOfSugar;
+            public CandiesSpecification(string type, string AmountOfSugar)
+            {
+                this.type = type;
+                this.AmountOfSugar = AmountOfSugar;
+            }
+            public void Specification()
+            {
+                Console.WriteLine($"Тип сладости -- {this.type}, Содержание сахара -- {this.AmountOfSugar}");
+            }
+        }
+    }
+}
---
+++ Lab05/Lab05/Present.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lab05
+{
+    public class Present
+    {
+        public List<Confectionery> list { get; set; }
+        private int Weight;
+        public int CountInPresent { get; set; }
+        public Present()
+        {
+            Weight = 1000;
+            CountInPresent = 0;
+            list = new List<Confectionery>();
+        }
+        public Present(int weight)
+        {
+            Weight = weight;
+            CountInPresent = 0;
+            list = new List<Confectionery>();
+        }
+        public Present(int weight, List<Confectionery> list)
+        {
+            Weight = weight;
+            CountInPresent = list.Count;
+            this.list = list;
+        }
+        public void AddElem(Confectionery candy)
+        {
+            list.Add(candy);
+            CountInPresent++;
+        }
+        public void DeleteElem(Confectionery candy)
+        {
+            if (!list.Contains(candy))
+                throw new ArgumentException();
+            list.Remove(candy);
+            CountInPresent--;
+
+        }
+        public void ShowList()
+        {
+            Console.WriteLine($"--------------\nДетский подарок\n" +
+                $"Количество конфет в подарке: {CountInPresent}");
+
+            foreach (var l in list)
+            {
+                Console.WriteLine(l.ToString());
+            }
+        }
+    }
+}
