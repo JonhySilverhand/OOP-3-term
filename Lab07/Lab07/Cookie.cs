407310a0bb16e25a6ad14f4597cc0c3246485f91
Vlad Smirnov <smirnovvlad313@gmail.com>
2022-10-15 01:08:47 +0300

    Лабораторная работа №7

---
+++ Lab07/Lab07/Cookie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07
+{
+    //4. Определить пользовательский класс, который будет использоваться в 
+    //качестве параметра обобщения.Для пользовательского типа взять класс из
+    //лабораторной №4 «Наследование». 
+    public class Cookie
+    {
+        public string Name { get; set; }
+        public Cookie()
+        {
+            Name = "Печенюшка";
+        }
+        public Cookie(string name)
+        {
+            Name = name;
+        }
+        public void TakeItem()
+        {
+            Console.WriteLine("Взята печенюшка");
+        }
+        public void BuyConfectionary()
+        {
+            Console.WriteLine("Куплена печенюшка");
+        }
+        public void GetConfectionary()
+        {
+            Console.WriteLine("Это печенюшка");
+        }
+        public override string ToString()
+        {
+            return $"Название - {this.Name}";
+        }
+    }
+}
---
+++ Lab07/Lab07/Interface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07
+{
+    interface Interface<T>
+    {
+        void Push(T s);
+        void Pop();
+        void CheckList();
+    }
+}
