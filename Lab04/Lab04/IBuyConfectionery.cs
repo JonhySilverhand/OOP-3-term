236d02111d9e40578c3bed31394dcc430587439d
Vlad Smirnov <smirnovvlad313@gmail.com>
2023-01-29 12:38:01 +0300

    Лабораторная работа №4

---
+++ Lab04/Lab04/IBuyConfectionery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab04
+{
+    interface IBuyConfectionery
+    {
+        void BuyConfectionary();
+        /* Добавьте в интерфейсы (интерфейс) и абстрактный класс одноименные 
+        методы.*/
+        public void GetConfectionary();
+    }
+}
