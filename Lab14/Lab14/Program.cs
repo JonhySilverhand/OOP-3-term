8c9fff3cc6b18e2d8b5fc6fb5e9e12d2179b5b11
Vlad Smirnov <smirnovvlad313@gmail.com>
2023-01-29 12:39:28 +0300

    Лабораторная работа №14

---
+++ Lab14/Lab14/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace Lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Определите и выведите на консоль/в файл все запущенные процессы:
+            //id, имя, приоритет, время запуска, текущее состояние,
+            //сколько всего времени использовал процессор и т.д.
+
+            var allProcess = Process.GetProcesses();
+
+            foreach(var proc in allProcess)
+            {
+                try
+                {
+                    Console.WriteLine(
+                        $"ID: {proc.Id}\n" + 
+                        $"Name: {proc.ProcessName}\n" +
+                        $"Priority: {proc.BasePriority}\n" +
+                        $"Time launch: {proc.StartTime}\n" +
+                        $"Current state: {proc.VirtualMemorySize64}\n" +
+                        $"CPU usage time: {proc.TotalProcessorTime}\n"
+                        );
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            //2. Исследуйте текущий домен вашего приложения: имя, детали конфигурации,
+            //все сборки, загруженные в домен.Создайте новый домен.Загрузите туда сборку.
+            //Выгрузите домен.
+            Console.WriteLine("----------------------------------------------------------");
+            AppDomain domain = AppDomain.CurrentDomain;
+
+            Console.WriteLine($"Domain name: {domain.FriendlyName}");
+            Console.WriteLine($"Configuration details: {domain.SetupInformation}");
+            Console.WriteLine("\nAll assemblies:\n");
+            Assembly[] asm = domain.GetAssemblies();
+            foreach (Assembly a in asm)
+                Console.WriteLine(a.GetName().Name);
+            Console.WriteLine();
+
+            try
+            {
+                AppDomain newD = AppDomain.CreateDomain("Новый домен");
+                newD.Load(@"D:\Лабораторные\3_семестр\ООП\Lab14\Lab14\obj\Debug\netcoreapp3.1\Lab14.dll");
+                AppDomain.Unload(newD);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+            }
+
+            /*3) Создайте в отдельном потоке следующую задачу расчета (можно сделать sleep для 
+            задержки) и записи в файл и на консоль простых чисел от 1 до n (задает пользователь). 
+            Вызовите методы управления потоком (запуск, приостановка, возобновление и т.д.) Во 
+            время выполнения выведите информацию о статусе потока, имени, приоритете, числовой 
+            идентификатор и т.д.*/
+
+            var firstThread = new Thread(ThreadInfo);
+            firstThread.Start(Thread.CurrentThread);
+            Thread.Sleep(1000);
+            int n;
+            Console.WriteLine("Введите n");
+            n = int.Parse(Console.ReadLine());
+
+            using (StreamWriter sw = new StreamWriter(@"D:\Лабораторные\3_семестр\ООП\Lab14\Lab14\\task2.txt", false))
+            {
+                for (var i = 1; i <= n; i++)
+                {
+                    var isSimple = true;
+                    for (var j = 2; j <= i / 2; j++)
+                        if (i % j == 0)
+                        {
+                            isSimple = false;
+                            break;
+                        }
+
+                    if (isSimple)
+                    {
+                        Console.Write($"{i} ");
+                        sw.Write($"{i} ");
+                    }
+                }
+            }
+            void ThreadInfo(object thread)
+            {
+                var currentThread = thread as Thread;
+                Console.WriteLine($"Имя потока: {currentThread.Name}");
+                Console.WriteLine($"Id: {currentThread.ManagedThreadId}");
+                Console.WriteLine($"Приоритет: {currentThread.Priority}");
+                Console.WriteLine($"Статус: {currentThread.ThreadState}");
+            }
+
+            /*4) Создайте два потока. Первый выводит четные числа, второй нечетные до n и 
+            записывают их в общий файл и на консоль. Скорость расчета чисел у потоков – разная.
+            a. Поменяйте приоритет одного из потоков. 
+            b. Используя средства синхронизации организуйте работу потоков, таким образом, чтобы:
+                i. выводились сначала четные, потом нечетные числа
+                ii. последовательно выводились одно четное, другое нечетное. */
+            Console.WriteLine("\n");
+            Console.WriteLine("Введите n");
+            n = int.Parse(Console.ReadLine());
+            Thread evenThread = new Thread(ShowEvenNumbers) { Priority = ThreadPriority.Highest } ;
+            Thread oddThread = new Thread(ShowOddNumbers);
+            evenThread.Start(n);
+            oddThread.Start(n);
+
+            void ShowEvenNumbers(object n)
+            {
+                Console.WriteLine("1st thread");
+                for (var i = 0; i < (int)n; i++)
+                {
+                    Thread.Sleep(10);
+                    if (i % 2 == 0)
+                    {
+                        Console.Write($"{i}st ");
+                    }
+                }
+            }
+            void ShowOddNumbers(object n)
+            {
+                Console.WriteLine("2nd thread");
+                for (var i = 0; i < (int)n; i++)
+                {
+                    Thread.Sleep(60);
+                    if (i % 2 != 0)
+                    {
+                        Console.Write($"{i}nd ");
+                    }
+                }
+            }
+            //четные, потом нечетные
+            Task.EvenOdd(n);
+            //одно четное - одно нечетное
+            Task.OneEvenOneOdd(n);
+             
+            //5. Придумайте и реализуйте повторяющуюся задачу на основе класса Timer
+
+            TimerCallback tm = new TimerCallback(CurrentTime);
+            Timer timer = new Timer(tm, null, 1000, 1000);
+            Thread.Sleep(6000);
+
+            void CurrentTime(object obj)
+            {
+                Console.WriteLine($"\nCurrent time: {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}");
+            }
+        }
+    }
+}
---
+++ Lab14/Lab14/Task.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace Lab14
+{
+    public static class Task
+    {
+        public static void EvenOdd(int n)
+        {
+            object locker = new object();
+            Thread.Sleep(n * 1000 / 3);
+            Thread evenNum = new Thread(ShowEvenNum);
+            Thread oddNum = new Thread(ShowOddNum);
+            evenNum.Start(n);
+            oddNum.Start(n);
+
+            void ShowEvenNum(object n)
+            {
+                bool acquiredLock = false;
+                try
+                {
+                    Console.WriteLine("\n3rd thread");
+                    Monitor.Enter(locker, ref acquiredLock);
+                    using(StreamWriter sw = new StreamWriter(@"D:\Лабораторные\3_семестр\ООП\Lab14\Lab14\task3.txt", true))
+                    {
+                        for(var i = 0; i < (int)n; i++)
+                        {
+                            Thread.Sleep(10);
+                            if(i % 2 == 0)
+                            {
+                                Console.Write($"{i} ");
+                                sw.Write($"{i} ");
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    if (acquiredLock)
+                        Monitor.Exit(locker);
+                }
+            }
+            void ShowOddNum(object n)
+            {
+                bool acquiredLock = false;
+                try
+                {
+                    Console.WriteLine("\n4th thread");
+                    Monitor.Enter(locker, ref acquiredLock);
+                    using (StreamWriter sw = new StreamWriter(@"D:\Лабораторные\3_семестр\ООП\Lab14\Lab14\task3.txt", true))
+                    {
+                        for (var i = 0; i < (int)n; i++)
+                        {
+                            Thread.Sleep(10);
+                            if (i % 2 != 0)
+                            {
+                                Console.Write($"{i} ");
+                                sw.Write($"{i} ");
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    if (acquiredLock)
+                        Monitor.Exit(locker);
+                }
+            }
+        }
+        public static void OneEvenOneOdd(int n)
+        {
+            Mutex mutex = new Mutex();
+            Thread.Sleep(n * 1000 / 3);
+            Thread evenNum2 = new Thread(ShowEvenNums);
+            Thread oddNum2 = new Thread(ShowOddNums);
+            evenNum2.Start(n);
+            oddNum2.Start(n);
+
+            void ShowEvenNums(object n)
+            {
+                Console.WriteLine("\n5th thread");
+                for (var i = 0; i < (int)n; i++)
+                {
+                    mutex.WaitOne();
+                    Thread.Sleep(10);
+                    if (i % 2 == 0)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                    mutex.ReleaseMutex();
+                }
+            }
+            void ShowOddNums(object n)
+            {
+                Console.WriteLine("\n6th thread");
+                for (var i = 0; i < (int)n; i++)
+                {
+                    mutex.WaitOne();
+                    Thread.Sleep(10);
+                    if (i % 2 != 0)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                    mutex.ReleaseMutex();
+                }
+            }
+        }
+    }
+}
